add9c6aeab988b33dbd99e68909c7be09eae1a20
Ivan Kalinin <to.wave.x@gmail.com>
2017-09-13 11:34:44 -0700

    Avoid mangling rules inside @keyframes

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ require('string.prototype.repeat');
 
 var CSS_ESCAPED_TAB = '\\9';
 
+
+function increaseSpecifityOfRule(rule, opts) {
+	rule.selectors = rule.selectors.map(function(selector) {
+		// Apply it to the selector itself if the selector is a `root` level component
+		// `html:not(#\\9):not(#\\9):not(#\\9)`
+		if(
+			selector === 'html' ||
+			selector === ':root' ||
+			selector === ':host' ||
+			selector === opts.stackableRoot
+		) {
+			return selector + opts.stackableRoot.repeat(opts.repeat);
+		}
+
+		// Otherwise just make it a descendant (this is what will happen most of the time)
+		// `:not(#\\9):not(#\\9):not(#\\9) .foo`
+		return opts.stackableRoot.repeat(opts.repeat) + ' ' + selector;
+	});
+
+	if(opts.overrideIds) {
+		if(
+			// If an id is in there somewhere
+			(new RegExp('#(?!' + escapeStringRegexp(CSS_ESCAPED_TAB) + ')')).test(rule.selector) ||
+			// Or it is an attribute selector with an id
+			(/\[id/).test(rule.selector)
+		) {
+			rule.walkDecls(function(decl) {
+				decl.important = true;
+			});
+		}
+	}
+}
+
+
 // Plugin that adds `:not(#\\9)` selectors to the front of the rule thus increasing specificity
 module.exports = postcss.plugin('postcss-increase-specificity', function(options) {
 	var defaults = {
@@ -22,36 +56,13 @@ module.exports = postcss.plugin('postcss-increase-specificity', function(options
 
 	return function(css) {
 		css.walkRules(function(rule) {
-			rule.selectors = rule.selectors.map(function(selector) {
-				// Apply it to the selector itself if the selector is a `root` level component
-				// `html:not(#\\9):not(#\\9):not(#\\9)`
-				if(
-					selector === 'html' ||
-					selector === ':root' ||
-					selector === ':host' ||
-					selector === opts.stackableRoot
-				) {
-					return selector + opts.stackableRoot.repeat(opts.repeat);
-				}
+			// Avoid adding additional selectors (stackableRoot) to descendant rules of @keyframe {}
+			// i.e. `from`, `to`, or `{number}%`
+			var isInsideKeyframes = rule.parent.type === 'atrule' && rule.parent.name === 'keyframes';
 
-				// Otherwise just make it a descendant (this is what will happen most of the time)
-				// `:not(#\\9):not(#\\9):not(#\\9) .foo`
-				return opts.stackableRoot.repeat(opts.repeat) + ' ' + selector;
-			});
-
-			if(opts.overrideIds) {
-				if(
-					// If an id is in there somewhere
-					(new RegExp('#(?!' + escapeStringRegexp(CSS_ESCAPED_TAB) + ')')).test(rule.selector) ||
-					// Or it is an attribute selector with an id
-					(/\[id/).test(rule.selector)
-				) {
-					rule.walkDecls(function(decl) {
-						decl.important = true;
-					});
-				}
+			if(!isInsideKeyframes) {
+				increaseSpecifityOfRule(rule, opts);
 			}
 		});
-
 	};
 });
